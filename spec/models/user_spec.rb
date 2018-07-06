cec5223d11e462912b4387d19a9b9d963833c080
kanndide <kanndide@gmail.com>
2018-07-06 22:26:14 +0300

    added rspec and stubbed out some tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  let(:user_student) {
+    User.create(
+      :username => "jsmith",
+      :password => "password",
+      :email => 5,
+    )
+  }
+
+  let(:admin) {
+    User.create(
+      :username => "jsmith",
+      :password => "password",
+      :email => 5,
+      :teacher => false,
+      :admin => true
+    )
+  }
+
+let(:user_teacher) {
+    User.create(
+      :username => "jsmith",
+      :password => "password",
+      :email => 5,
+      :teacher => true
+    )
+  }
+
+ let(:user_info) {
+     UserInfo.create(
+       :first_name => “John”
+       :last_name => “Smith”
+       :bio => “I am a Junior studying         premed.”
+       :user_id => 1
+     )
+   }
+
+  it "is valid with a username, password, email" do
+    expect(user_student).to be_valid
+  end
+
+  it "is not valid without a password" do
+    expect(User.new(name: "Name")).not_to be_valid
+  end
+
+  it "is valid with an admin boolean" do
+    expect(admin).to be_valid
+  end
+
+  it "is valid with an teacher boolean" do
+    expect(user_teacher).to be_valid
+  end
+
+  it "defaults to admin => false & teacher => false" do
+    expect(user_student.admin).to eq(false)
+    expect(user_student.teacher).to eq(false)
+  end
+
+end
