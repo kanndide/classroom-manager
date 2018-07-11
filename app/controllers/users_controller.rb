1c6875abf243acfe33be0bcc82a921da10ea2883
kanndide <kanndide@gmail.com>
2018-07-11 14:41:12 +0300

    user new/create added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
 
 	def new
-		@user = User.new
+		if logged_in?
+			redirect_to user_path(current_user)
+		else
+			@user = User.new
+		end
+	end
+
+	def create
+		if User.exists?(:username => params[:username])
+			flash[:danger] = "Sorry, that username already exists."
+			redirect_to new_user_path
+		elsif params[:password] == params[:password_confirmation]
+			@user = User.new(user_params)
+		end
+		
+		if @user.save
+			log_in(@user)
+			redirect_to user_path(@user)
+		else
+			redirect_to new_user_path
+		end
 	end
 
 	def show
@@ -14,6 +34,12 @@ class UsersController < ApplicationController
 
 	def update
 
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :password, :email, :first_name, :last_name, :bio)
 	end
 
 	
