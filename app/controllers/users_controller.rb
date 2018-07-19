4a028bc523d0ac303834c8b8f5e872e2a8540f4e
kanndide <kanndide@gmail.com>
2018-07-19 23:41:20 +0300

    user_params fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:username, :password, :email, :first_name, :last_name, :bio, :teacher)
+		params.require(:user).permit(:username, :password, :email, :first_name, :last_name, :bio, :teacher, :admin)
 	end
 
 	def require_login
