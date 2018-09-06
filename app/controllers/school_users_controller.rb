5f576c5e5f02266f8d15413b645578efed59fd98
kanndide <kanndide@gmail.com>
2018-09-06 18:59:56 +0300

    changed controller to render json object

--- app/controllers/school_users_controller.rb
+++ app/controllers/school_users_controller.rb
@@ -9,7 +9,7 @@ class SchoolUsersController < ApplicationController
 		@school = School.find(params[:school_user][:school_id])
 		SchoolUser.create(:school_id => @school.id, :user_id => current_user.id)
 		flash[:success] = "You have have been associated with #{@school.name}."
-		redirect_to user_path(current_user)
+		render json: @school
 	end
 
 end
