28265a2f900f849a440dae4bbf8d18e702779a53
kanndide <kanndide@gmail.com>
2018-09-06 19:01:29 +0300

    added user show to json capability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,12 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		respond_to do |format|
+		    format.html
+		    format.json{
+		      render :json => @user.to_json
+		    }
+  		end
 	end
 
 	def edit
