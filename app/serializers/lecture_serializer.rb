af43f83f016a75cef6b93504475e958130eeb259
kanndide <kanndide@gmail.com>
2018-09-07 13:21:22 +0300

    added year attribute to serializer

--- app/serializers/lecture_serializer.rb
+++ app/serializers/lecture_serializer.rb
@@ -1,5 +1,5 @@
 class LectureSerializer < ActiveModel::Serializer
-  attributes :id, :name, :time, :day, :location, :semester
+  attributes :id, :name, :time, :day, :location, :semester, :year
   belongs_to :school
   has_many :user_lectures
   has_many :users, through: :user_lectures
