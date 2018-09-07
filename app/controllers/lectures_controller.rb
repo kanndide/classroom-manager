c58aec9ad0744c3c2d8d326ec6eea26a88a3811d
kanndide <kanndide@gmail.com>
2018-09-07 13:22:25 +0300

    added redner json to create and show routes

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -18,10 +18,11 @@ class LecturesController < ApplicationController
 			redirect_to new_user_lecture_path(current_user)
 		else
 			@lecture = Lecture.new(lecture_params)
+			@user = current_user
 			if @lecture.save
 				UserLecture.create(:user_id => current_user.id, :lecture_id => @lecture.id) unless current_user.admin && !current_user.teacher
 				flash[:success] = "#{@lecture.name} has been successfully added."
-				redirect_to user_lectures_path(current_user)
+				render json: @lecture
 			else
 				flash[:danger] = "Something went wrong. Please try again."
 				render :new
