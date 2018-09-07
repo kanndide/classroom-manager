ce531581345dc4d919b89f7fdd7e81ad49781f24
kanndide <kanndide@gmail.com>
2018-09-07 13:23:24 +0300

    created function to create new lecture and append object

--- app/assets/javascripts/lectures.js
+++ app/assets/javascripts/lectures.js
@@ -1,8 +1,20 @@
-
-function showLecture(lectureID) {
-	
-};
-
-function createShowLectureLink(object) {
-	
+function showNewLecture() {
+	$('form').submit(function(event) {
+		event.preventDefault();
+		params = $(this).serialize()
+		newLecture = $.post('/lectures', params)
+		newLecture.done(function(data) {
+			lecture = data["data"]["attributes"]
+			id = Number(data["data"]["id"])
+			userId = Number(data["data"]["relationships"]["users"]["data"][0]["id"])
+			$('#newLectureName').append(lecture.name)
+			$('#showNewLecture').append(`<p>${lecture.time}</p><br>`)
+			$('#showNewLecture').append(`<p>${lecture.day}</p><br>`)
+			$('#showNewLecture').append(`<p>${lecture.semester}</p><br>`)
+			$('#showNewLecture').append(`<p>${lecture.location}</p><br>`)
+			// $('#showNewLecture').append(`<p>${lecture.school}</p><br>`)
+			$('#showNewLecture').append(`<p>${lecture.year}</p><br>`)
+			$('#showNewLecture').append(`<a href="/users/${userId}/lectures/${id}/edit">Edit Class</a><br>`)
+		});
+	});
 };
