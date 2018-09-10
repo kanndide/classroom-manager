c6f77d85620f6ff982111c3e84133f639cec1bf8
kanndide <kanndide@gmail.com>
2018-09-10 18:23:13 +0300

    added model object and prototype method

--- app/assets/javascripts/lectures.js
+++ app/assets/javascripts/lectures.js
@@ -1,11 +1,20 @@
+function Lecture(data) {
+	this.id = Number(data.id);
+	this.name = data["attributes"].name;
+	this.semester = data["attributes"].semester;
+	this.time = data["attributes"].time;
+	this.location = data["attributes"].location;
+	this.day = data["attributes"].day;
+	this.year = Number(data["attributes"].year);
+};
+
 function showNewLecture() {
 	$('form').submit(function(event) {
 		event.preventDefault();
 		params = $(this).serialize()
 		newLecture = $.post('/lectures', params)
 		newLecture.done(function(data) {
-			lecture = data["data"]["attributes"]
-			id = Number(data["data"]["id"])
+			lecture = new Lecture(data["data"])
 			userId = Number(data["data"]["relationships"]["users"]["data"][0]["id"])
 			$('#newLectureName').append(lecture.name)
 			$('#showNewLecture').append(`<p>${lecture.time}</p><br>`)
@@ -14,7 +23,7 @@ function showNewLecture() {
 			$('#showNewLecture').append(`<p>${lecture.location}</p><br>`)
 			// $('#showNewLecture').append(`<p>${lecture.school}</p><br>`)
 			$('#showNewLecture').append(`<p>${lecture.year}</p><br>`)
-			$('#showNewLecture').append(`<a href="/users/${userId}/lectures/${id}/edit">Edit Class</a><br>`)
+			$('#showNewLecture').append(`<a href="/users/${userId}/lectures/${lecture.id}/edit">Edit Class</a><br>`)
 		});
 	});
 };
@@ -26,29 +35,26 @@ function getLectures() {
 			var today = new Date()
 			var year = today.getFullYear()
 			var lecturesArray = data["data"]
-			$('#userPanelHeader')[0].innerHTML = '<h1>Scheduled Lectures</h1>'
-			$('#userPanelBody')[0].innerHTML = ''
-			$('#userPanelBody').append(`<h3>${year}</h3>`)
-			for (let lecture of lecturesArray) {
-				lectureYear = Number(lecture["attributes"].year)
-				if(lectureYear === year) {
-					$('#userPanelBody').append(`<a href="/lectures/${Number(lecture.id)}">
-						${lecture["attributes"].semester} - ${lecture["attributes"].name}, ${lecture["attributes"].time}</a><br>`)
+			$('#panelHeader')[0].innerHTML = '<h1>Scheduled Lectures</h1>'
+			$('#panelBody')[0].innerHTML = ''
+			$('#panelBody').append(`<h3>${year}</h3>`)
+			for (let data of lecturesArray) {
+				lecture = new Lecture(data)
+				if(lecture.year === year) {
+					$('#panelBody').append(lecture.formatLectureShowLink())
 				};
 			};
-			$('#userPanelBody').append(`<h3>${year + 1}</h3>`)
-			for (let lecture of lecturesArray) {
-				lectureYear = Number(lecture["attributes"].year)
-				if(lectureYear === year + 1) {
-					debugger
-					$('#userPanelBody').append(`<a href="/lectures/${Number(lecture.id)}">
-						${lecture["attributes"].semester} - ${lecture["attributes"].name}, ${lecture["attributes"].time}</a><br>`)
+			$('#panelBody').append(`<h3>${year + 1}</h3>`)
+			for (let data of lecturesArray) {
+				lecture = new Lecture(data)
+				if(lecture.year === year + 1) {
+					$('#panelBody').append(lecture.formatLectureShowLink())
 				};
 			};
 		});
 	});
 };
 
-function sortLecturesByYear(lecturesArray) {
-
+Lecture.prototype.formatLectureShowLink = function() {
+	return `<a href="/lectures/${this.id}">${this.semester} - ${this.name}, ${this.time}</a><br>`
 };
