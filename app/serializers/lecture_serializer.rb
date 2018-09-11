818d5ad61fd057d9fb1d014d84b0762de8304e38
kanndide <kanndide@gmail.com>
2018-09-11 21:32:58 +0300

    removed through relationship

--- app/serializers/lecture_serializer.rb
+++ app/serializers/lecture_serializer.rb
@@ -1,6 +1,7 @@
 class LectureSerializer < ActiveModel::Serializer
   attributes :id, :name, :time, :day, :location, :semester, :year
   belongs_to :school
-  has_many :user_lectures
-  has_many :users, through: :user_lectures
+  has_many :users
 end
+
+
