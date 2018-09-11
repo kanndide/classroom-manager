64f529f040ce3db5c1f46e52b2d1bbf8574a50c3
kanndide <kanndide@gmail.com>
2018-09-11 21:34:38 +0300

    removed join table relationship

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :bio, :teacher, :admin
-  has_many :user_lectures
-  has_many :lectures, through: :user_lectures
-  has_many :school_users
-  has_many :schools, through: :school_users
+  has_many :lectures
+  has_many :schools
 end
