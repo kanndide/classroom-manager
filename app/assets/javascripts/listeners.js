3d96a7f99e265f6a21e86cbd15fc362b42787fb0
kanndide <kanndide@gmail.com>
2018-09-11 21:32:13 +0300

    tidied up and added turbolinks insted of 'ready'

--- app/assets/javascripts/listeners.js
+++ app/assets/javascripts/listeners.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
-	attachListeners()
-})
+$(document).on('turbolinks:load', attachListeners);
 
 function attachListeners() {
-	$('#schoolUserSubmit').on('click', () => createAssociatedSchools());
-	$('#schoolFindLectures').on('click', () => showSchoolLectures());
-	$('#newLectureFormButton').on('click', () => showNewLecture());
-	$('#viewSchedule').on('click', () => getLectures()); 
+	$('#schoolUserSubmit').on('click', createAssociatedSchools)
+	$('#schoolFindLectures').on('click', showSchoolLectures)
+	$('#newLectureFormButton').on('click', showNewLecture)
+	$('#viewSchedule').on('click', getLectures)
 }
+
+
+// Turbolink Load?
